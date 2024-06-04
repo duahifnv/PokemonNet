d8ed2ebcdcd40b593acba5f352a87afb6768090b
duahifnv <max135603@gmail.com>
2024-06-04 17:11:55 +0300

    Добавлена запись имени файла в файл с результатом

--- net/__init__.py
+++ net/__init__.py
@@ -1,7 +1,7 @@
 import json
 from urllib import request
 
-from flask import Flask, flash, request, redirect, render_template, url_for
+from flask import Flask, flash, request, redirect, render_template, url_for, Response
 from werkzeug.utils import secure_filename
 
 import net.predict
@@ -33,12 +33,15 @@ def process_file():
         flash('No file part')
         return redirect(request.url)
     file = request.files['file']
+    print(file)
     if file.filename == '':
         flash('No selected file')
         return redirect(request.url)
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
+        print(filename)
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        print(filepath)
         file.save(filepath)
         # Предикт keras по картинке
         prediction = net.predict.Predict(filepath)
@@ -46,23 +49,28 @@ def process_file():
         result = prediction.predict()
         # Парсим данные
         result = parse_data(result)
+        result["name"] = file.filename.split('.')[0]
+        print(result["name"])
+        os.remove(filepath)
         # Записываем результат в текстовый файл
         with open('predictions/predictions.txt', 'w') as f:
             f.write(str(result))
-        return redirect(request.url)
-
+        return Response(status=200)
 
 @app.route('/upload', methods=['GET'])
 def show_predict():
     # Если еще не было предикта
     if os.stat('predictions/predictions.txt').st_size == 0:
         return redirect(request.url)
-    with open('predictions/predictions.txt', 'r') as f:
+    with open('predictions/predictions.txt', 'r+') as f:
         result = f.read()
     json_acceptable_string = result.replace("'", "\"")
     result = json.loads(json_acceptable_string)  # DICTIONARY
-    return render_template("prediction.html", generation=result["generation"],
-                           type1=result["type1"], type2=result["type2"])
+    return render_template("prediction.html",
+                           label=result["name"],
+                           generation=result["generation"],
+                           type1=result["type1"],
+                           type2=result["type2"])
 
 
 if __name__ == '__main__':
